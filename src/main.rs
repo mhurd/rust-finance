9cef546983399dfe8b70b5143d80fdd822a9fe72
mhurd <mike@mhurd.com>
2019-05-29 16:24:37 +0100

    Some leg logging examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Foobar!!");
+    use leg::*;
+    use std::env;
+
+    let args: Vec<String> = env::args().collect();
+    let args_string = format!("{:?}", args);
+    let scope = "Program args";
+
+    info(&args_string, Some(&scope), None);
+    success(&args_string, Some(&scope), None);
+    warn(&args_string, Some(&scope), None);
+    error(&args_string, Some(&scope), None);
+    wait(&args_string, Some(&scope), None);
+    done(&args_string, Some(&scope), None);
+
 }
